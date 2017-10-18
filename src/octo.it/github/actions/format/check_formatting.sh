7537b4697f407193e82022d2c0e0b273be8e6b35
Florian Forster <ff@octo.it>
2017-10-18 14:23:27 +0200

    check_formatting.sh: Download Git repo on first run

--- src/octo.it/github/actions/format/check_formatting.sh
+++ src/octo.it/github/actions/format/check_formatting.sh
@@ -4,14 +4,22 @@ set -e
 #set -x
 
 if [[ $# != 4 ]]; then
-	echo "Usage: $0 <owner> <repo> <branch> <base>"
-	exit 1
+  echo "Usage: $0 <owner> <repo> <branch> <base>"
+  exit 1
 fi
 
 declare -r OWNER="$1"; shift
 declare -r REPO="$1"; shift
 declare -r BRANCH="$1"; shift
 declare -r BASE="$1"; shift
+
+if [[ ! -d /var/lib/check_formatting ]]; then
+  mkdir /var/lib/check_formatting
+  cd /var/lib/check_formatting
+  git clone -q --origin github git://github.com/collectd/collectd.git
+fi
+
+cd /var/lib/check_formatting/collectd
 
 git remote update github >/dev/null
 git fetch -f -q "git://github.com/${OWNER}/${REPO}" "${BRANCH}:check_formatting"
@@ -21,7 +29,7 @@ git checkout -q check_formatting
 #declare -r FILE_FRMT="$(tempfile)"
 
 git diff --name-only "github/${BASE}...FETCH_HEAD" | egrep '\.(c|h|proto)$' | while read f; do
-  clang-format-3.8 -style=file -i "${f}"
+  clang-format -style=file -i "${f}"
 done
 
 declare -i HAVE_CHANGES=0
@@ -31,7 +39,7 @@ if ! git diff --quiet; then
 fi
 
 git reset -q --hard
-git checkout -q '-'
+git checkout -q master
 git branch -q -D check_formatting
 
 exit $HAVE_CHANGES
