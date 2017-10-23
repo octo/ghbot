ed083f82c4f3e8f0f28d472ca86c417cec3e3504
Florian Forster <ff@octo.it>
2017-10-23 10:56:26 +0200

    deploy.sh: Cleanup.

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,6 @@ set -e
 declare -r ACCT='octo@verplant.org'
 declare -r PROJ='collectd-github-bot'
 
-declare VERSION="v$(date +%s)"
-declare TAG="gcr.io/collectd-github-bot/github-bot:${VERSION}"
+declare -r VERSION="v$(date +%s)"
 
-docker build -t "${TAG}" .
-gcloud docker --account="${ACCT}" --project="${PROJ}" -- push "${TAG}"
-
-gcloud app deploy --account="${ACCT}" --project="${PROJ}" --version="${VERSION}" --image-url "${TAG}"
+gcloud app deploy --account="${ACCT}" --project="${PROJ}" --version="${VERSION}"
