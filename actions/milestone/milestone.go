a1c5dabb5b252f52732a3fb847ead53283725b64
Florian Forster <ff@octo.it>
2017-11-02 09:38:09 +0100

    milestone action: Add logging.

--- actions/milestone/milestone.go
+++ actions/milestone/milestone.go
@@ -11,6 +11,7 @@ import (
 	"github.com/google/go-github/github"
 	"github.com/octo/ghbot/client"
 	"github.com/octo/ghbot/event"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
@@ -25,10 +26,13 @@ func handler(ctx context.Context, e *github.PullRequestEvent) error {
 	c := client.New(ctx, client.DefaultOwner, client.DefaultRepo)
 	pr := c.WrapPR(e.PullRequest)
 
+	log.Debugf(ctx, "checking if a milestone should be set for %v", pr)
+
 	ref := pr.PullRequest.Base.GetRef()
 
 	// This is likely a PR for the master branch.
 	if !strings.HasPrefix(ref, "collectd-") {
+		log.Debugf(ctx, "milestone: no, not a feature branch: %q", ref)
 		return nil
 	}
 	v := strings.TrimPrefix(ref, "collectd-")
@@ -41,6 +45,7 @@ func handler(ctx context.Context, e *github.PullRequestEvent) error {
 
 	// A milestone has already been set.
 	if i.Issue.Milestone != nil {
+		log.Debugf(ctx, "milestone: no, already set to %q", i.Issue.Milestone.GetTitle())
 		return nil
 	}
 
@@ -55,5 +60,6 @@ func handler(ctx context.Context, e *github.PullRequestEvent) error {
 		}
 	}
 
+	log.Debugf(ctx, "milestone: no, milestone %q not found", v)
 	return nil
 }
